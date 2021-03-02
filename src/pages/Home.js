915c95e57aad951f2bd52bd12be15d1b155ae7aa
ivegoie <axos.music@gmail.com>
2021-03-02 18:21:01 +0100

    showing upcoming games and styling it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
 
 // REDUX
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+
+// COMPONENTS
+import Game from "../components/Game";
+
+// STYLING AND ANIMATION
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   // FETCH GAMES
@@ -10,12 +17,39 @@ const Home = () => {
   useEffect(() => {
     dispatch(loadGames());
   }, [dispatch]);
-
+  // GETTING DATA
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <GameList>
+      <h1>Upcoming Games</h1>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            key={game.id}
+            image={game.background_image}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 50vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 2rem;
+  grid-row-gap: 3rem;
+`;
 
 export default Home;
