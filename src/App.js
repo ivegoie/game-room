b09c098572cd71da944dc594df40373b2198d8fa
ivegoie <axos.music@gmail.com>
2021-03-02 16:16:11 +0100

    fetching games in state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <h1>Hello Game Room</h1>
