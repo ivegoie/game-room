66c3f17df2742dcea7cd19028a366ac0bd932340
ivegoie <axos.music@gmail.com>
2021-03-02 16:03:27 +0100

    sucessfully added Redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 // REDUX SETUP
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancer(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
